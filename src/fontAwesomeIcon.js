0ea3269f85a3c89bf1f3c530a0775a0c16da6d6b
이유준 사원 <yjlee@seowoninfo.com>
2022-02-16 15:41:31 +0900

    add: fontAwesome icon ragule 무료아이콘 패키치 추가

--- src/fontAwesomeIcon.js
+++ src/fontAwesomeIcon.js
@@ -5,5 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 // 설치했던 아이콘파일에서 해당 아이콘만 불러옵니다. (전체 아이콘 모듈 추가)
 import { fas } from "@fortawesome/free-solid-svg-icons";
-library.add(fas);
+import { far } from "@fortawesome/free-regular-svg-icons";
+
+library.add(fas, far);
 Vue.component("fa-icon", FontAwesomeIcon);
